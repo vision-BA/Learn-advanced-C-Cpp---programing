bcacbd7a80a0a68f40e19f894115c09bfe49f858
VISION-BA <brightathuman65@gmail.com>
2026-03-09 20:57:24 +0300

    adding first c++ program

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"enter a"<<endl;
+    cin>>a;
+    cout<<"enter b"<<endl;
+    cin>>b;
+    cout<<"The product of those numbers is "<<a*b<<endl;
+    return 0;
+}
+//
+// Created by MUFIMBA on 09-Mar-26.
+//
