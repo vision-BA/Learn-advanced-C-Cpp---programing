07286153acf00df37dd952c567b78808f49ca9d6
VISION-BA <brightathuman65@gmail.com>
2026-03-12 23:21:44 +0300

    doing basic  math operation

---
+++ 4mathoperation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x,y;
+cout<<"Enter x: "<<endl;
+cin>>x;
+cout<<"Enter y: "<<endl;
+cin>>y;
+cout<<"The sum of " << "x and y is: "  << x+y<<endl;
+    system("pause>0");
+    // pause to execute next code
+int p=6;
+int u=3;
+cout<<"the product is this is "<< p*u;
+    system("pause>0");
+}
+
