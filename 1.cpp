0c495a4e1c8291c4b3532e6b6a18b481681fe5b7
VISION-BA <brightathuman65@gmail.com>
2026-03-12 21:42:20 +0300

    setting C debugging enviroment

--- 1.cpp
+++ 1.cpp
@@ -1,14 +1,6 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-    int a,b;
-    cout<<"enter a"<<endl;
-    cin>>a;
-    cout<<"enter b"<<endl;
-    cin>>b;
-    cout<<"The product of those numbers is "<<a*b<<endl;
+int main(){
+    cout << "Hello World!" << endl;
     return 0;
 }
-//
-// Created by MUFIMBA on 09-Mar-26.
-//
--- variable.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by MUFIMBA on 10-Mar-26.
-//
