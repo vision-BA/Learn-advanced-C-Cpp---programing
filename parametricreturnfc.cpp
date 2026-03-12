11e095faa1cedb457a279d7680bf1ee498e9ad07
VISION-BA <brightathuman65@gmail.com>
2026-03-12 23:56:57 +0300

    adding non parameterized funtion

---
+++ parametricreturnfc.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+float divide(int x, int y){
+    return x/y;
+}
+
+int main(){
+cout<<divide(40,8);
+
+    system("pause>0");
+
+}
---
+++ variabletpc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+float x=5.3, y=4.8;
+cout<<" the product of x and y is: "<<x*y<<endl;
+cout<<sizeof(x)<<endl;
+// pause to next execution 
+ system("pause>0");
+char c='f';
+cout<<c<<endl;
+cout<<sizeof(c)<<endl;
+// pause to next execution 
+    system("pause>0");
+bool f=false;
+cout<<f<<endl;
+    system("pause>0");
+
+}
