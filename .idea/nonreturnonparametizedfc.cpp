d53f2c233baa6fa1337353e499b914a5c3de8c9b
VISION-BA <brightathuman65@gmail.com>
2026-03-14 01:08:18 +0300

    modifies function

---
+++ .idea/nonreturnonparametizedfc.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void say(){
+     string firstname="bright";
+     char x='e';
+    cout<<"my name is "<<firstname<<endl;
+    cout<<x<<endl;
+} 
+void add(int x,int y){
+    cout<<x+y<<endl;
+}
+int main(){
+    say();
+    add(6,8);
+    system("pause>0");
+}
+
---
+++ .idea/nonreturnparametizedfc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void divide(float x,float y){
+    cout<<"the quoetient of x and y is "<<x/y<<endl;
+}
+int main() {
+    divide(7.9,3.2);
+    system("pause>0");
+}
